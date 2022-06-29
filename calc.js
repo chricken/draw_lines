40426d578aee0f25c0892c37aad82a4dbdc789ed
C. Heisch <max@mustermann.de>
2022-06-29 13:47:08 +0200

    neue Render-Methode

--- calc.js
+++ calc.js
@@ -3,6 +3,17 @@
 const calc = {
     createNumber(min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min)
+    },
+    createColor({
+        minSat = 80,
+        maxSat = 100,
+        minLight = 40,
+        maxLight = 60,
+        minHue = 0,
+        maxHue = 360
+    } = {}) {
+        const cn = calc.createNumber;
+        return `hsl(${cn(minHue, maxHue)}, ${cn(minSat, maxSat)}%, ${cn(minLight, maxLight)}%)`;
     }
 }
 
--- draw.js
+++ draw.js
@@ -3,15 +3,17 @@
 import calc from './calc.js';
 
 const draw = {
+
     c: false,
     ctx: false,
     path: [],
     pressed: false,
-    iterations: 8,
+    iterations: 16,
     deviation: 10,
-    lineWidth: .5,
-    delayMeasurement: 50,
-    render() {
+    lineWidth: 1,
+    delayMeasurement: 30,
+
+    render2() {
         let ctx = draw.ctx;
         ctx.lineWidth = draw.lineWidth;
         ctx.clearRect(0, 0, draw.c.width, draw.c.height);
@@ -20,13 +22,79 @@ const draw = {
 
         for (let j = 0; j < draw.iterations; j++) {
             let [x, y] = draw.path[0].map(val => val + calc.createNumber(-(draw.deviation * 10) / 10, (draw.deviation * 10) / 10));
+
             ctx.moveTo(x, y)
+
             for (let i = 1; i < draw.path.length; i++) {
-                [x, y] = draw.path[i].map(val => val + calc.createNumber(-(draw.deviation * 10) / 10, (draw.deviation * 10) / 10));
-                ctx.lineTo(x, y)
+                let dev = draw.deviation;
+                let [x0, y0] = draw.path[i - 1];
+                let [x1, y1] = draw.path[i];
+
+                let abstand = Math.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2);
+
+                dev = dev / 30 * abstand;
+
+
+                [x, y] = draw.path[i].map(val =>
+                    val + calc.createNumber(-(dev * 10) / 10, (dev * 10) / 10)
+                );
+                ctx.lineTo(x, y);
+
+                [x, y] = draw.path[i].map(val =>
+                    val + calc.createNumber(-(dev * 10) / 10, (dev * 10) / 10)
+                );
+                ctx.moveTo(x, y);
             }
         }
         ctx.stroke();
+
+        /* Debugging-Punkte
+        for (let i = 0; i < draw.path.length; i++) {
+            ctx.fillRect(...draw.path[i], 3, 3);
+        }
+        */
+    },
+
+    render() {
+        let ctx = draw.ctx;
+        ctx.lineWidth = draw.lineWidth;
+        ctx.clearRect(0, 0, draw.c.width, draw.c.height);
+
+
+        for (let j = 0; j < draw.iterations; j++) {
+            ctx.beginPath();
+            let [x, y] = draw.path[0].map(val => val + calc.createNumber(-(draw.deviation * 10) / 10, (draw.deviation * 10) / 10));
+
+            ctx.moveTo(x, y)
+
+            for (let i = 1; i < draw.path.length; i++) {
+                let dev = draw.deviation;
+                let [x0, y0] = draw.path[i - 1];
+                let [x1, y1] = draw.path[i];
+
+                let abstand = Math.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2);
+
+                dev = dev / 30 * abstand;
+
+
+                [x, y] = draw.path[i].map(val =>
+                    val + calc.createNumber(-(dev * 10) / 10, (dev * 10) / 10)
+                );
+
+                ctx.lineTo(x, y)
+            }
+
+            ctx.strokeStyle = calc.createColor({
+                minSat: 10,
+                maxSat: 30,
+                minLight: 30,
+                maxLight: 50,
+                minHue: 300,
+                maxHue: 360
+            })
+
+            ctx.stroke();
+        }
 
         /* Debugging-Punkte
         for (let i = 0; i < draw.path.length; i++) {
